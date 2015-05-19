1cb48c31e619c10009d8a6481e9aa57c4bb7a557
Chris Lill <chris_lill_uk@hotmail.com>
2015-05-19 23:14:25 +0100

    Get and clean data

---
+++ plot1.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(lubridate)
+
+# First check whether the file exists, and if not, create it
+if (!file.exists("household_power_consumption.txt")) {
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url, "PowerConsumption.zip")
+    unzip("PowerConsumption.zip")
+}
+
+# If the dataset for these plots doesn't exist, create it
+if (!exists("power.feb")) {
+    power.classes <- c("character", "character", rep("numeric",7))
+    power.all <- read.csv("household_power_consumption.txt", 
+                          sep = ";",
+                          na.strings = "?",
+                          colClasses = power.classes)
+    power.feb <- power.all[power.all$Date %in% c("1/2/2007", "2/2/2007"),]
+    rm(power.all)
+    
+    # Convert to a datetime object
+    power.feb <- power.feb %>%
+        mutate(Datetime = paste(Date, Time)) %>%
+        mutate(Datetime = dmy_hms(Datetime))
+}
